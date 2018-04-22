ac20960900b4a7f5b152509c4115b55424964ebf
Taicho <taicho.nv@gmail.com>
2018-04-22 12:41:49 +0900

    Make SignUp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
   has_secure_password
 end
