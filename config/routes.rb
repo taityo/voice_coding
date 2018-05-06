63be290d77195b8bdca283e604c857062792d24d
taicho <taicho.nv@gmail.com>
2018-05-06 22:29:26 +0900

    Start User Pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get "/about" => "static_pages#about"
   get "/signup" => "users#new"
+  post "/signup" => "users#create"
   resources :users
 end
